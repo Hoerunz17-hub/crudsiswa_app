3d82893fca185c68099fdf47d461340202ce6f85
Hoerunz17-hub <khoerunmaulana1@gmail.com>
2025-07-30 08:46:08 +0700

    project pertama

---
+++ app/Models/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'clas_id',
+        'photo',
+        'nisn',
+        'alamat',
+        'no_handphone',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    // Relasi ke tabel classes
+    public function class()
+    {
+        return $this->belongsTo(Classes::class, 'clas_id');
+    }
+}

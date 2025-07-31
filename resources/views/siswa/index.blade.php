cec84d534884d5ccbf09d40d7589b4b30ae3c6f1
Hoerunz17-hub <khoerunmaulana1@gmail.com>
2025-07-31 11:34:45 +0700

    style (siswa) menambahkan view siswa

---
+++ resources/views/siswa/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halaman utama</title>
+</head>
+<body>
+    <h1>Halaman Utama Siswa</h1>
+    <h3>Daftar Siswa</h3>
+    <hr>
+    <a href="/siswa/create">Tambah siswa</a>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <thead>
+            <tr>
+                <th>NO</th>
+                <th>NAMA</th>
+                <th>KELAS</th>
+                <th>NISN</th>
+                <th>ALAMAT</th>
+                <th>PHOTO</th>
+                <th>OPTION</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                     <a href="">DELETE</a>
+                     <a href="">EDIT</a>
+                     <a href="">DETAIL</a>
+                </td>
+
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
